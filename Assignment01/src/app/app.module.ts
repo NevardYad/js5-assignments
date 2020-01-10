b4481845df5f0dfdae7cbf9b3bd6b368d38a40aa
NevardYad <draven.day01@stclairconnect.ca>
2020-01-10 16:58:34 -0500

    Intro from week 1 Complete

--- Assignment01/src/app/app.module.ts
+++ Assignment01/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SafePipe } from './safe.pipe';
+import { ContentCardComponent } from './content-card/content-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafePipe,
+    ContentCardComponent
   ],
   imports: [
     BrowserModule
